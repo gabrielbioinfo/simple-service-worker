06df49672ce2b262ac9dcba761775e630fb48e20
Gabriel Espindola <gabrielbioinfo@gmail.com>
2018-10-11 13:26:34 -0300

    acertando sw

--- app/www/serviceWorker.js
+++ app/www/serviceWorker.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    resolve(navigator.serviceWorker.register('/serviceWorker.js'))
+    navigator.serviceWorker.register('/serviceWorker.js')
     .then(()=>{})
     .catch((err)=>{
         console.log('service worker error: ', err);
